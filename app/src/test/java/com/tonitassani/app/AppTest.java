8a95bdd488eeea9d4d88fcedad5daedbbb6b3f4a
Toni Tassani <atassani@gmail.com>
2014-08-07 05:49:51 +0200

    Added AspectJ LTW, but not working yet

--- app/src/test/java/com/tonitassani/app/AppTest.java
+++ app/src/test/java/com/tonitassani/app/AppTest.java
@@ -2,7 +2,6 @@ package com.tonitassani.app;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
