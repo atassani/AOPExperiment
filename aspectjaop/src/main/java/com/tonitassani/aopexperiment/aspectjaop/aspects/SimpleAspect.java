ddcc09faf87c1d7e3e0efd319bedd3fefb6cb73d
Toni Tassani <atassani@gmail.com>
2014-08-13 17:28:16 +0200

    Experiments with LTW. Not working

---
+++ aspectjaop/src/main/java/com/tonitassani/aopexperiment/aspectjaop/aspects/SimpleAspect.java
@@ -0,0 +1,15 @@
+package com.tonitassani.aopexperiment.aspectjaop.aspects;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class SimpleAspect {
+
+	@Around("execution(* com.tonitassani.aopexperiment.module.ModuleWithoutDefaultConstructor.sum(..)) && args(a,b)") 
+	public int sumAdvice(ProceedingJoinPoint joinPoint, int a, int b) throws Throwable {
+		System.out.println("INTERCEPTED ****************************************");
+		return (Integer)joinPoint.proceed(new Object[] {a + 2, b + 3});
+	}
+}
